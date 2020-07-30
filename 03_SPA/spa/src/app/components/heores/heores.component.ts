61ad0144947bcc3b571ebe1b87e7f6d5ab7faed2
Adrián González <yo.robot.1990@gmail.com>
2020-07-29 19:55:11 -0500

    Primeras integraciones del proyecto SPA - resolve #51

---
+++ 03_SPA/spa/src/app/components/heores/heores.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heores',
+  templateUrl: './heores.component.html'
+})
+export class HeoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
